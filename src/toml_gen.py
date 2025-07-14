78362433ab662ac123dd10eb0749f65076f87906
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2025-07-14 13:17:00 -0300

    ordenando

--- src/toml_gen.py
+++ src/toml_gen.py
@@ -42,7 +42,7 @@ classifiers = [
 ]
 license = "GPL-3.0-only WITH Classpath-Exception-2.0 OR BSD-3-Clause"
 license-files = ["LICENSE"]
-keywords = ["writing", "translate"]
+keywords = ["FAPESP", "research", "funding", "scholarships", "grants", "Brazil"]
 dependencies = [
     "PyQt5",
     "requests",
